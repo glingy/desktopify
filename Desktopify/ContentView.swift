3a86cc8a4a9aa3def383f3890e37976ff9781c5a
Gregory Ling <0b0100011101101100@gmail.com>
2020-03-16 10:26:59 -0700

    Initial Commit

---
+++ Desktopify/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Desktopify
+//
+//  Created by Gregory Ling on 3/16/20.
+//  Copyright © 2020 Gregory Ling. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
